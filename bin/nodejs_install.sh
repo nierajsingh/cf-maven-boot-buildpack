02862a8f128f45fcd5cc8f185f7c03a8d8ac2d37
nsingh <nsingh@pivotal.io>
2016-08-16 15:18:10 -0700

    Initial download and install of nodejs

--- bin/nodejs_install.sh
+++ bin/nodejs_install.sh
@@ -1,1 +1,24 @@
-echo "HELLO YOU ARE INSTALLING NODEJS"
+echo "Intalling Node.js from separate script"
+nodeJsUrl=https://nodejs.org/dist/v4.5.0/node-v4.5.0-linux-x86.tar.xz
+appDir=$1
+cacheDir=$2
+##############################################################################################
+# Node.js setup
+##############################################################################################
+echo ===== setting up Node.js =====
+cd $cacheDir
+if [ -f node-v4.5.0-linux-x86.tar.xz]; then
+    echo "Found node.js install in cache"
+else
+    wget $jdkUrl -O node-v4.5.0-linux-x86.tar.xz # would be nice to use -N but it doesn't work for this url.
+fi
+
+cd ${appDir}
+mkdir nodejs
+cd nodejs
+echo "Unzipping to " `pwd`
+tar xzf ${cacheDir}/node-v4.5.0-linux-x86.tar.xz
+
+nodeJsDirName=`ls` # there should be just one sub dir here so this shoud work to find its name.
+echo "nodeJsDirName = " $nodeJsDirName
+#export JAVA_HOME=$appDir/jdk/$javaDirName
